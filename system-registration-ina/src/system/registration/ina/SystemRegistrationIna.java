00c235da829fab61bad4dc6f3979e8a8e34f43fa
Royden Ceciliano <roydench31@gmail.com>
2022-02-07 11:03:25 -0600

    Import Java project

---
+++ system-registration-ina/src/system/registration/ina/SystemRegistrationIna.java
@@ -0,0 +1,12 @@
+package system.registration.ina;
+
+public class SystemRegistrationIna {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
