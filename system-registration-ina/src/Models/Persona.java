ce3a553c8f4f088236c719bda28fbe2a1fa9ce13
Anchmm24 <98854192+Anchmm24@users.noreply.github.com>
2022-02-07 12:53:22 -0600

    Create Persona.java

---
+++ system-registration-ina/src/Models/Persona.java
@@ -0,0 +1,14 @@
+package Models;
+
+/**
+ *
+ * @author angyc
+ */
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private String cedula;
+    private String direccion;
+    private String correo;
+    private String edad;
+}
