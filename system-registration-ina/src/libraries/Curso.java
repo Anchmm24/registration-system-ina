a55182a01d5771b9b45a7984094665e856c0dc97
Royden Ceciliano <roydench31@gmail.com>
2022-02-07 11:22:40 -0600

    Create Curso class

---
+++ system-registration-ina/src/libraries/Curso.java
@@ -0,0 +1,67 @@
+package libraries;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author royden
+ */
+public class Curso {
+    String nombre;
+    byte cupoMax;
+    byte creditos;
+    Docente docenteEncargado;
+    List<Date> horarios;
+
+    public Curso() {
+    }
+
+    public Curso(String nombre, byte cupoMax, byte creditos, Docente docenteEncargado, List<Date> horarios) {
+        this.nombre = nombre;
+        this.cupoMax = cupoMax;
+        this.creditos = creditos;
+        this.docenteEncargado = docenteEncargado;
+        this.horarios = horarios;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public byte getCupoMax() {
+        return cupoMax;
+    }
+
+    public void setCupoMax(byte cupoMax) {
+        this.cupoMax = cupoMax;
+    }
+
+    public byte getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(byte creditos) {
+        this.creditos = creditos;
+    }
+
+    public Docente getDocenteEncargado() {
+        return docenteEncargado;
+    }
+
+    public void setDocenteEncargado(Docente docenteEncargado) {
+        this.docenteEncargado = docenteEncargado;
+    }
+
+    public List<Date> getHorarios() {
+        return horarios;
+    }
+
+    public void setHorarios(List<Date> horarios) {
+        this.horarios = horarios;
+    }
+}
