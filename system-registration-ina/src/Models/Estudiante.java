c29a28f8a8deeabf8f14dcd9a10e3655b9a67992
Royden Ceciliano <roydench31@gmail.com>
2022-02-07 13:14:41 -0600

    Update Estudiante.java

--- system-registration-ina/src/Models/Estudiante.java
+++ system-registration-ina/src/Models/Estudiante.java
@@ -9,7 +9,59 @@ import java.util.List;
 public class Estudiante extends Persona{
     byte gradoActual;
     boolean repiteAnio;
-    boolean reqAdecuacion;
+    boolean adecuacion;
     boolean beca;
-    List<Curso>
+    List<Curso> cursos;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(byte gradoActual, boolean repiteAnio, boolean reqAdecuacion, boolean beca, List<Curso> cursos) {
+        this.gradoActual = gradoActual;
+        this.repiteAnio = repiteAnio;
+        this.adecuacion = reqAdecuacion;
+        this.beca = beca;
+        this.cursos = cursos;
+    }
+
+    public byte getGradoActual() {
+        return gradoActual;
+    }
+
+    public void setGradoActual(byte gradoActual) {
+        this.gradoActual = gradoActual;
+    }
+
+    public boolean isRepiteAnio() {
+        return repiteAnio;
+    }
+
+    public void setRepiteAnio(boolean repiteAnio) {
+        this.repiteAnio = repiteAnio;
+    }
+
+    public boolean isAdecuacion() {
+        return adecuacion;
+    }
+
+    public void setAdecuacion(boolean adecuacion) {
+        this.adecuacion = adecuacion;
+    }
+
+    public boolean isBeca() {
+        return beca;
+    }
+
+    public void setBeca(boolean beca) {
+        this.beca = beca;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }    
+    
 }
